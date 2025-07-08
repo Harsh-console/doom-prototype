10f9331a679b8d900031daeea5536672b05dc828
Harsh-console <harsh.console004@gmail.com>
2025-07-08 15:33:31 +0530

    Remove Code_Practice from repo but keep it locally

--- Code_Practice/Computer_Graphics/finclude_with_ppm/view_image.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-# 🛡️ Disable the decompression bomb protection (safe here)
-Image.MAX_IMAGE_PIXELS = None
-
-# 📂 Load and view the image
-img = Image.open(r"C:\Users\USER\projects\helloworld\Code_Practice\Computer_Graphics\cube.ppm")
-img.show()
